f0716dc95e901774ea4e5d33724703c5b7b63c34
sanjiv sahayam <sanjsmailbox@gmail.com>
2017-03-10 01:42:46 +1100

    add sha256 for artifact

--- HomebrewFormula/provide.rb
+++ HomebrewFormula/provide.rb
@@ -2,8 +2,8 @@ class Provide < Formula
   desc "Serve http content quickly"
   homepage "https://github.com/ssanj/provide"
   version "0.0.1"
-  url "https://github.com/ssanj/provide/releases/download/v#{version}/dabble", :using => :nounzip
-  sha256 ""
+  url "https://github.com/ssanj/provide/releases/download/v#{version}/provide", :using => :nounzip
+  sha256 "570521dd168a52582a6f2da80fe923cf5032f05d8a4af69692a5bad2b67b7e88"
   head "https://github.com/ssanj/provide.git"
 
   if build.head?
@@ -15,7 +15,7 @@ class Provide < Formula
     if build.head?
       # ENV.java_cache
       system "sbt", "assembly"
-      bin.install "target/scala-2.12/provide*" => "provide"
+      bin.install "target/scala-2.12/provide" => "provide"
     else
       bin.install "provide" => "provide"
     end
